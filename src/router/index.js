38647d98f200f743e4f94258314c032850dd48cb
Me <skolskymailherencsar@gmail.com>
2026-01-17 19:54:32 +0100

    Github Pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
